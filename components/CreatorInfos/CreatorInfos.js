a345681fb8f55454ea19f225660bc6477b849e1a
LucasBargas <lucas.bargasoficial@gmail.com>
2022-06-06 11:27:36 -0300

    Infos update

--- components/CreatorInfos/CreatorInfos.js
+++ components/CreatorInfos/CreatorInfos.js
@@ -6,6 +6,7 @@ const CreatorInfos = () => {
       <p>
         Esta calculadora foi desenvolvida com <span>♥</span> por <a href="https://projetoslucasbargas.com/" target="_blank" rel="noopener noreferrer">Lucas Bargas</a>
       </p>
+      <a href="https://github.com/LucasBargas/simple_calculator" target="_blank" rel="noopener noreferrer">Clique aqui para visualizar o repositório no GitHub</a>
     </section>
   )
 }
